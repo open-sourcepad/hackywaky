7dcfb96179f888e9d013c4e941f85206448e09d6
Lester Celestial <lesterc@sourcepad.com>
2014-10-03 19:11:46 +0800

    removed protect from forgery

--- app/controllers/slack_response_controller.rb
+++ app/controllers/slack_response_controller.rb
@@ -1,4 +1,6 @@
 class SlackResponseController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def create
     SlackResponseSaver.new(params).save
     render nothing: true
